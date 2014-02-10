8a028044dd74a101f71629219603cf20bf99dbc8
lifeng <lifeng198931@163.com>
2014-02-10 22:45:52 +0800

    view add

---
+++ BabelSpeaker/src/com/babelspeaker/activity/news/MainNewsActivity.java
@@ -0,0 +1,33 @@
+package com.babelspeaker.activity.news;
+
+import android.os.Bundle;
+
+import com.babelspeaker.common.BaseActivity;
+
+public class MainNewsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+    
+    
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+    }
+    
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+    }
+    
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+}
